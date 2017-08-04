844b8704c120ab54e5f051d2b95cc7a417900fd0
Revamp Software, Inc. <softwarevamp@gmail.com>
2017-08-03 18:47:34 -0700

    integrate with i18n
    Signed-off-by: Revamp Software, Inc. <softwarevamp@gmail.com>

--- src/PHPixie/Template/Extensions/Extension/HTML.php
+++ src/PHPixie/Template/Extensions/Extension/HTML.php
@@ -27,6 +27,17 @@ class HTML implements \PHPixie\Template\Extensions\Extension
 
     public function escape($string)
     {
+        // TODO: translate with i18n
+        $translations = array(
+            'phase' => '阶段',
+            'region' => '地域',
+            'date' => '时间'
+        );
+        
+        if (isset($translations[$string])) {
+            $string = $translations[$string];
+        }
+        
         return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
     }
     
